61c747cdaf76480968aed841a2b61e9e92ce7e76
Shivam Patel <163013822+2004shivampatel@users.noreply.github.com>
2024-10-16 19:43:17 +0530

    Delete EventRepository.java

--- EventRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Event.Repository;
-
-import com.example.Event.Model.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event, Long> {
-}
